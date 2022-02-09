4ea763592823d9bb68bdf992b3fda456090e6b5f
Ahmed Kamal <info@ahmedmkamal.com>
2022-02-09 23:12:37 +0200

    Update cors.go

--- server/middleware/cors.go
+++ server/middleware/cors.go
@@ -1,6 +1,7 @@
 package middleware
 
 import (
+	"net/http"
 	"strconv"
 	"strings"
 
@@ -16,10 +17,13 @@ type Cors struct {
 }
 
 // HandleRequest handles the incoming requests.
-func (ref *Cors) HandleRequest(_ *server.HttpRequest, res *server.HttpResponse) error {
+func (ref *Cors) HandleRequest(req *server.HttpRequest, res *server.HttpResponse) error {
 	res.Header.Set("Access-Control-Allow-Origin", strings.Join(ref.Origin, ","))
 	res.Header.Set("Access-Control-Allow-Methods", strings.Join(ref.Methods, ","))
 	res.Header.Set("Access-Control-Allow-Headers", strings.Join(ref.Headers, ","))
 	res.Header.Set("Access-Control-Allow-Credentials", strconv.FormatBool(ref.AllowCredentials))
+	if req.Request.Method == http.MethodOptions {
+		return res.Send([]byte(""))
+	}
 	return nil
 }
