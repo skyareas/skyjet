a23e0a0c7a7f144726d7ee580ffbc3c6fccf46f8
Ahmed Kamal <info@ahmedmkamal.com>
2022-05-21 05:04:04 +0200

    Static file handler & Path match by prefix

--- examples/static/main.go
+++ examples/static/main.go
@@ -6,9 +6,9 @@ import (
 
 func main() {
 	app := skyjet.SharedApp()
-	app.Get("/", func(req *skyjet.HttpRequest, res *skyjet.HttpResponse) error {
-		return res.SendFile("index.html")
-	})
 	app.Use("/assets", skyjet.Static())
+	app.Get("/", func(req *skyjet.HttpRequest, res *skyjet.HttpResponse) error {
+		return res.SendFile("views/index.html")
+	})
 	app.Run()
 }
